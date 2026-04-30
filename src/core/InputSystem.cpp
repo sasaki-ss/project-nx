b5fd25380f3599f4967ba706e7fd4f57898eb52e
Kuroneko <kaityou5555@gmail.com>
2026-05-01 08:29:20 +0900

    [fix] null安全性向上対応

--- src/core/InputSystem.cpp
+++ src/core/InputSystem.cpp
@@ -1,10 +1,11 @@
-#include "InputSystem.h"
+﻿#include "InputSystem.h"
 
 namespace nx {
 namespace core {
 namespace input {
 
 bool InputSystem::init() {
+	// Keyは0からの連番である前提
 	int key_count = static_cast<int>(Key::Count);
 	for (int i = 0; i < key_count; ++i) {
 		current_state[static_cast<Key>(i)] = KeyState::Released;
@@ -14,9 +15,12 @@ bool InputSystem::init() {
 	return true;
 }
 
-void InputSystem::set_input_source(std::unique_ptr<InputBase> input_module) {
+bool InputSystem::set_input_source(std::unique_ptr<InputBase> input_module) {
+	if (input_module == nullptr) {
+		return false;
+	}
 	this->input_module = std::move(input_module);
-	this->input_module->init();
+	return this->input_module->init();
 }
 
 void InputSystem::update() {
