75348e317593ce45ea6d2010086c22d2b8535615
Kuroneko <kaityou5555@gmail.com>
2026-05-01 11:21:57 +0900

    [add] Componentクラスの追加

---
+++ src/core/Component.h
@@ -0,0 +1,26 @@
+﻿#ifndef CORE_OBJECT_COMPONENT
+#define CORE_OBJECT_COMPONENT
+
+#include "InputSystem.h"
+
+namespace nx {
+namespace core {
+namespace object {
+
+class Component final {
+public:
+	Component() = default;
+	~Component() = default;
+	auto input() -> input::InputSystem& {
+		return input_system;
+	}
+private:
+	input::InputSystem input_system;
+};
+
+
+}
+}
+}
+
+#endif // !CORE_OBJECT_COMPONENT
